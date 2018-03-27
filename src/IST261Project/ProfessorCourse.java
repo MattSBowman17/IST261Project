3cbbafca13af07a734e6fef764818c8615817713
ctg5117 <ctg5117@BKGAIGE205-30.win.psu.edu>
2018-03-27 14:43:42 -0400

    Project Created

--- src/IST261Project/ProfessorCourse.java
+++ src/IST261Project/ProfessorCourse.java
@@ -9,61 +9,79 @@ public class ProfessorCourse {
     private int Course_CourseID;
 
     private int Package_Package_ID;
-
+    
+    public ProfessorCourse(int Professor_ProfessorID, int Course_CourseID, int Package_Package_ID)
+    {
+        this.Professor_ProfessorID = Professor_ProfessorID;
+        this.Course_CourseID = Course_CourseID;
+        this.Package_Package_ID = Package_Package_ID;
+    }
+    
     /**
      * @return the ProfessorCourseID
      */
-    public String getProfessorCourseID() {
+    public String getProfessorCourseID()
+    {
         return ProfessorCourseID;
     }
 
     /**
      * @param ProfessorCourseID the ProfessorCourseID to set
      */
-    public void setProfessorCourseID(String ProfessorCourseID) {
+    public void setProfessorCourseID(String ProfessorCourseID)
+    {
         this.ProfessorCourseID = ProfessorCourseID;
     }
 
     /**
      * @return the Professor_ProfessorID
      */
-    public int getProfessor_ProfessorID() {
+    public int getProfessor_ProfessorID()
+    {
         return Professor_ProfessorID;
     }
 
     /**
      * @param Professor_ProfessorID the Professor_ProfessorID to set
      */
-    public void setProfessor_ProfessorID(int Professor_ProfessorID) {
+    public void setProfessor_ProfessorID(int Professor_ProfessorID)
+    {
         this.Professor_ProfessorID = Professor_ProfessorID;
     }
 
     /**
      * @return the Course_CourseID
      */
-    public int getCourse_CourseID() {
+    public int getCourse_CourseID()
+    {
         return Course_CourseID;
     }
 
     /**
      * @param Course_CourseID the Course_CourseID to set
      */
-    public void setCourse_CourseID(int Course_CourseID) {
+    public void setCourse_CourseID(int Course_CourseID) 
+    {
         this.Course_CourseID = Course_CourseID;
     }
 
     /**
      * @return the Package_Package_ID
      */
-    public int getPackage_Package_ID() {
+    public int getPackage_Package_ID()
+    {
+        
         return Package_Package_ID;
     }
 
     /**
      * @param Package_Package_ID the Package_Package_ID to set
      */
-    public void setPackage_Package_ID(int Package_Package_ID) {
+    public void setPackage_Package_ID(int Package_Package_ID) 
+    {
         this.Package_Package_ID = Package_Package_ID;
     }
+    
+    
     
 }
